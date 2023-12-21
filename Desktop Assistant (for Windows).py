de7d18eecb65fd02f67d1797b529a0ff3bf5d610
Dinesh_dk_176 <151514446+Dineshdk176@users.noreply.github.com>
2023-12-21 22:50:39 +0530

    Update Desktop Assistant (for Windows).py

--- Desktop Assistant (for Windows).py
+++ Desktop Assistant (for Windows).py
@@ -1,3 +1,4 @@
+import subprocess as sp
 import os
 import sys
 import datetime as dt
@@ -5,6 +6,7 @@ import speech_recognition as sr
 import wikipedia
 import webbrowser
 import pyttsx3
+import pywhatkit as yt
 
 def welcome():
     time=int(dt.datetime.now().hour)
@@ -15,13 +17,22 @@ def welcome():
     else:
     	speak("Good Evening ")
     speak("How Can I Help You !")
+    speak("Are You want to Intract with Keyboard.? (Y/N)")
+    f_back=input().lower()
+    if(f_back=='y'):
+        return "y"
+    elif(f_back=='n'):
+        return "n"
+
 
 def speak(audio):
     print("AI :",audio)
+    if(len(audio)==45):
+        audio=audio[0:37]
     engine=pyttsx3.init()
     engine.setProperty('voice','en-in')
     speed=engine.getProperty('rate')
-    engine.setProperty('rate',speed-60)
+    engine.setProperty('rate',speed-50)
     engine.say(audio,name=None )
     engine.runAndWait()
     
@@ -40,89 +51,169 @@ def r_voice():
         speak("Say Again Please..")
 
 if __name__=="__main__":
-    os.system('clear')
-    welcome()
-    while True:
+    f=welcome()
+    while(f=='n'):
         query=r_voice()
         if(query==None):
             continue
-        query=query.lower()
+        query=query.lower().replace(" ","")
+        
         if('goodbye' in query):
             speak("Ok Good Bye Thakyou for Using Me..")
             break
-        elif "open vs code" in query:
-            os.system("code")
+        
+        elif ("text editor" in query or "notepad" in query):
+            sp.run(["notepad.exe"])
             condition = input("Are you continue (Y/N):").lower()
             if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
                 break
 
-        elif "open chrome" in query:
-            os.system("google-chrome")
+        elif ("open folder" in query):
+            sp.run(["explorer.exe"])
             condition = input("Are you continue (Y/N):").lower()
             if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
                 break
 
-        elif "open camera" in query:
-            os.system("cheese")
-            condition = input("Are you continue (Y/N):").lower()
-            if(condition == 'n'):
-                break
-
-        elif "text editor" in query:
-            os.system("gedit")
-            condition = input("Are you continue (Y/N):").lower()
-            if(condition == 'n'):
-                break
-
-        elif "open folder" in query:
-            os.system("open /nishanth-path")
-            condition = input("Are you continue (Y/N):").lower()
-            if(condition == 'n'):
-                break
-
-        elif "open vlc" in query:
-            os.system("vlc")
-            condition = input("Are you continue (Y/N):").lower()
-            if(condition == 'n'):
-                break
-
-        elif "search" in query:
+        elif ("search" in query):
             query=query.replace("search","")
-            query=query.replace(" ","")
             webbrowser.open(f"google.com/search?q={query}")
             condition = input("Are you continue (Y/N):").lower()
             if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
                 break
 
-        elif "youtube" in query:
+        elif ("youtube" in query):
             speak("opening youtube...")
             query=query.replace("youtube","")
-            query=query.replace(" ","")
             webbrowser.open(f"youtube.com/results?search_query={query}")
             condition = input("Are you continue (Y/N):").lower()
             if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
                 break
 
-        elif "wikipedia" in query:
+        elif ("wikipedia" in query):
             speak("searching in wikipedia")
             query=query.replace("wikipedia","")
             results=wikipedia.summary(query,sentences=3)
             speak("According to wikipedia")
             speak(results)
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
 
-        elif "play" in query:
+        elif ("play" in query):
             speak("Starting...")
             query=query.replace("play","")
             yt.playonyt(query)
             condition = input("Are you continue (Y/N):").lower()
             if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
                 break
 
-        elif "open" in query:
+        elif ("open" in query):
             query=query.replace("open","")
-            query=query.replace(" ","")
             speak(f"opening {query}...")
             webbrowser.open(f"{query}.com")
             condition = input("Are you continue (Y/N):").lower()
             if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
                 break
+            
+        '''elif("open vs code" in query):
+            os.system("code")
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+                
+            elif ("open chrome" in query):
+            sp.run(["chrome.exe"])
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+        '''
+        
+    while(f=='y'):
+        query=input("Waiting for Input...").lower().replace(" ","")
+        if('goodbye' in query):
+            speak("Ok Good Bye Thakyou for Using Me..")
+            break
+        
+        elif ("text editor" in query or "notepad" in query):
+            sp.run(["notepad.exe"])
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+
+        elif ("open folder" in query):
+            sp.run(["explorer.exe"])
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+
+        elif ("search" in query):
+            query=query.replace("search","")
+            webbrowser.open(f"google.com/search?q={query}")
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+
+        elif ("youtube" in query):
+            speak("opening youtube...")
+            query=query.replace("youtube","")
+            webbrowser.open(f"youtube.com/results?search_query={query}")
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+
+        elif ("wikipedia" in query):
+            speak("searching in wikipedia")
+            query=query.replace("wikipedia","")
+            results=wikipedia.summary(query,sentences=3)
+            speak("According to wikipedia")
+            speak(results)
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+
+        elif ("play" in query):
+            speak("Starting...")
+            query=query.replace("play","")
+            yt.playonyt(query)
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+
+        elif ("open" in query):
+            query=query.replace("open","")
+            speak(f"opening {query}...")
+            webbrowser.open(f"{query}.com")
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+            
+        '''elif("open vs code" in query):
+            os.system("code")
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+                
+            elif ("open chrome" in query):
+            sp.run(["chrome.exe"])
+            condition = input("Are you continue (Y/N):").lower()
+            if(condition == 'n'):
+                speak("Ok Good Bye Thakyou for Using Me..")
+                break
+        '''
