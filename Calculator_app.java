9642853a5211287a5737b4446bc70dde5ffe4713
Dinesh_dk_176 <151514446+Dineshdk176@users.noreply.github.com>
2024-02-20 21:46:29 +0530

    Update Calculator_app.java

--- Calculator_app.java
+++ Calculator_app.java
@@ -1,69 +1,73 @@
 //package org.example;
 import java.awt.*;
+import javax.swing.*;
 import java.awt.event.*;
-public class Calculator_app extends Frame implements ActionListener{
-    String val="";
-    int check=0,n;
-    public double[] num=new double[2];
-    int i=0;
+class Calculator extends JFrame implements ActionListener
+{
+    String val = "";
+    int check = 0 , n ; //check  --> Check number of Operations
+    public double[] num = new double[2];
+    int i = 0; //Index Value of num Array.
     String pros;
     Label l1;
-    TextField t=new TextField("0");
-    Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,ba,bs,bm,bd,be,clr,del;
-    Menu m=new Menu("Menu");
-    MenuBar mb=new MenuBar();
-    MenuItem i1,i2;
-    Font fnt=new Font("Courier",Font.BOLD,16);
-    public Calculator_app()
+    JTextField t = new JTextField("0");
+    JButton b0 , b1 , b2 , b3 , b4 , b5 , b6 , b7 , b8, b9 , dot , ba , bs , bm , bd , be , clr , del;
+    JMenu m= new JMenu("Menu");
+    JMenuBar mb = new JMenuBar();
+    JMenuItem  i1;
+    Font fnt = new Font("Courier",Font.BOLD,16);
+    Calculator()
     {
-        b0=new Button("0");
-        b1=new Button("1");
-        b2=new Button("2");
-        b3=new Button("3");
-        b4=new Button("4");
-        b5=new Button("5");
-        b6=new Button("6");
-        b7=new Button("7");
-        b8=new Button("8");
-        b9=new Button("9");
-        ba=new Button("+");
-        bs=new Button("-");
-        bm=new Button("x");
-        bd=new Button("/");
-        be=new Button("=");
-        clr =new Button("AC");
-        del =new Button("Del");
-        i1=new MenuItem("Scientific");
-        i2=new MenuItem("Quit");
-        i2.addActionListener((event) -> System.exit(0));
-        l1=new Label("CALCULATOR");
+        b0 = new JButton("0");
+        b1 = new JButton("1");
+        b2 = new JButton("2");
+        b3 = new JButton("3");
+        b4 = new JButton("4");
+        b5 = new JButton("5");
+        b6 = new JButton("6");
+        b7 = new JButton("7");
+        b8 = new JButton("8");
+        b9 = new JButton("9");
+        dot = new JButton(".");
+        ba = new JButton("+");
+        bs = new JButton("-");
+        bm = new JButton("x");
+        bd = new JButton("/");
+        be = new JButton("=");
+        clr = new JButton("AC");
+        del = new JButton("DL");
+        i1= new JMenuItem("Quit");
+        i1.addActionListener((event) -> System.exit(0));
+        l1 = new Label("CALCULATOR");
 
         l1.setFont(fnt);
+        l1.setBackground(Color.black);
+        l1.setForeground(Color.white);
         t.setFont(fnt);
-        l1.setBounds(60,60,120,18);
-        t.setBounds(15,100,205,35);
-        b9.setBounds(100,150,35,35);
-        b8.setBounds(60,150,35,35);
-        b7.setBounds(20,150,35,35);
-        b6.setBounds(100,190,35,35);
-        b5.setBounds(60,190,35,35);
-        b4.setBounds(20,190,35,35);
-        b3.setBounds(100,230,35,35);
-        b2.setBounds(60,230,35,35);
-        b1.setBounds(20,230,35,35);
-        b0.setBounds(25,270,105,35);
-        ba.setBounds(140,150,35,35);
-        bs.setBounds(140,190,35,35);
-        bm.setBounds(140,230,35,35);
-        bd.setBounds(140,270,35,35);
-        clr.setBounds(180,150,35,55);
-        del.setBounds(180,210,35,55);
-        be.setBounds(180,270,35,35);
+        l1.setBounds(80,10,135,35);
+        t.setBounds(25,60,235,35);
+        b9.setBounds(120,120,50,35);
+        b8.setBounds(65,120,50,35);
+        b7.setBounds(10,120,50,35);
+        b6.setBounds(120,160,50,35);
+        b5.setBounds(65,160,50,35);
+        b4.setBounds(10,160,50,35);
+        b3.setBounds(120,200,50,35);
+        b2.setBounds(65,200,50,35);
+        b1.setBounds(10,200,50,35);
+        b0.setBounds(10,240,105,35);
+        dot.setBounds(120,240,50,35);
+        ba.setBounds(175,120,50,35);
+        bs.setBounds(175,160,50,35);
+        bm.setBounds(175,200,50,35);
+        bd.setBounds(175,240,50,35);
+        clr.setBounds(230,120,50,55);
+        del.setBounds(230,180,50,55);
+        be.setBounds(230,240,50,35);
 
         m.add(i1);
-        m.add(i2);
         mb.add(m);
-        setMenuBar(mb);
+        setJMenuBar(mb);
         add(l1);
         add(t);
         add(b9);
@@ -76,6 +80,7 @@ public class Calculator_app extends Frame implements ActionListener{
         add(b2);
         add(b1);
         add(b0);
+        add(dot);
         add(ba);
         add(bs);
         add(bm);
@@ -84,6 +89,7 @@ public class Calculator_app extends Frame implements ActionListener{
         add(del);
         add(be);
         add(new Canva());
+        pack();
 
         action(0,b0);
         action(1,b1);
@@ -102,17 +108,17 @@ public class Calculator_app extends Frame implements ActionListener{
         bs.addActionListener(this);
         bm.addActionListener(this);
         bd.addActionListener(this);
-
-//        pack();
+ 
         setTitle("CALCULATOR");
-        setSize(235,360);
+        setSize(300,400);
+        setBackground(Color.black);
         setLayout(null);
         setVisible(true);
-        l1.setBackground(Color.black);
-        l1.setForeground(Color.white);
     }
-    public void actionPerformed(ActionEvent e){
-        if (e.getSource()==be) {
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getSource()==be) 
+        {
             double tot=switch (pros){
                 case "add" -> num[0]+num[1];
                 case "sub" -> num[0]-num[1];
@@ -127,33 +133,45 @@ public class Calculator_app extends Frame implements ActionListener{
             t.setText(val);
             val="";
             check=0;
-        } else if (e.getSource()==del) {
-            if(val.endsWith("+") || val.endsWith("-") || val.endsWith("x") || val.endsWith("/")){
+        } 
+        else if (e.getSource()==del) 
+        {
+            if(val.endsWith("+") || val.endsWith("-") || val.endsWith("x") || val.endsWith("/"))
+            {
                 pros="";
                 check=0;
             }
-            else{
+            else
+            {
                 num[i] = (int) (num[i]/10.0);
             }
             val=val.substring(0,(val.length())-1);
             t.setText(val);
 
-        } else if (e.getSource()==clr) {
-            val="";
-            num[0]=0;
-            num[1]=0;
-            check=0;
+        } 
+        else if (e.getSource()==clr) 
+        {
+            num[0]*=0;
+            num[1]*=0;
+            check*=0;
+            val="0";
+            i=0;
             t.setText(val);
+            val="";
 
-        } else if (e.getSource()==ba ) {
-            if(check!=1){
+        }
+        else if (e.getSource()==ba ) 
+        {
+            if(check!=1)
+            {
                 pros="add";
                 val+="+";
                 i=1;
                 check=1;
                 t.setText(val);
             }
-            else{
+            else
+            {
                 t.setText("One Operation can do!");
                 i=0;
                 num[0]=0;
@@ -161,15 +179,19 @@ public class Calculator_app extends Frame implements ActionListener{
                 val="";
                 check=0;
             }
-        } else if (e.getSource()==bs) {
-            if(check!=1) {
+        } 
+        else if (e.getSource()==bs) 
+        {
+            if(check!=1) 
+            {
                 pros = "sub";
                 val += "-";
                 i = 1;
                 check = 1;
                 t.setText(val);
             }
-            else{
+            else
+            {
                 t.setText("One Operation can do!");
                 i=0;
                 num[0]=0;
@@ -177,15 +199,19 @@ public class Calculator_app extends Frame implements ActionListener{
                 val="";
                 check=0;
             }
-        } else if (e.getSource()==bm) {
-            if(check!=1) {
+        } 
+        else if (e.getSource()==bm) 
+        {
+            if(check!=1) 
+            {
                 pros = "mul";
                 val += "x";
                 i = 1;
                 check = 1;
                 t.setText(val);
             }
-            else{
+            else
+            {
                 t.setText("One Operation can do!");
                 i=0;
                 num[0]=0;
@@ -193,15 +219,19 @@ public class Calculator_app extends Frame implements ActionListener{
                 val="";
                 check=0;
             }
-        } else if (e.getSource()==bd) {
-            if(check!=1) {
+        } 
+        else if (e.getSource()==bd) 
+        {
+            if(check!=1) 
+            {
                 pros = "div";
                 val += "/";
                 i = 1;
                 check = 1;
                 t.setText(val);
             }
-            else{
+            else
+            {
                 t.setText("One Operation can do!");
                 i=0;
                 num[0]=0;
@@ -211,10 +241,11 @@ public class Calculator_app extends Frame implements ActionListener{
             }
         }
     }
-    public void action(int number,Button obj)
+    public void action(int number,JButton obj)
     {
         obj.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e)
+            {
                 n=number;
                 val+=Integer.toString(number);
                 num[i]*=10;
@@ -223,17 +254,20 @@ public class Calculator_app extends Frame implements ActionListener{
             }
         });
     }
+}
 
+public class Calculator_app {
     public static void main(String[] args)
     {
-        Calculator_app c=new Calculator_app();
+        Calculator c=new Calculator();
     }
 }
+
 class Canva extends Canvas
 {
     public Canva()
     {
         setBackground(Color.black);
-        setSize(400,400);
+        setSize(1200,1000);
     }
 }
