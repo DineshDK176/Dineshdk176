492c2e6188bc334eee7851efb9b401322e6c6f55
Dinesh_dk_176 <151514446+DineshDK176@users.noreply.github.com>
2024-04-02 20:23:50 +0530

    feat : Errors and Interface is Updated

--- SDAW with GUI.py
+++ SDAW with GUI.py
@@ -10,10 +10,9 @@ from itertools import count
 class GifLabel(Label):
 
     text = ''
-    con = ''
-    f_back,entry = 0,0
-    
-    
+    f_back,con  = 0,0
+
+    #To Load the GIF image as a Label
     def __init__(self, master=None,**kwargs):
         super().__init__(master, **kwargs)
         self.frames = []
@@ -54,7 +53,6 @@ class GifLabel(Label):
     engine = pyttsx3.init()
     engine.setProperty('voice', 'en-in')
     engine.setProperty('rate', engine.getProperty('rate') - 40)
-    mic = sr.Recognizer()
     
     def speak(self, query,con=None):
         label = Label(root,bg='black')
@@ -66,8 +64,16 @@ class GifLabel(Label):
                 pass
         label.config(text=f"AI: {txt}",fg="white")
         label.pack()
-        self.engine.say(txt)
-        self.engine.runAndWait()
+        
+        try:
+                
+            self.engine.say(txt)
+            self.engine.runAndWait()
+            
+        except RuntimeError:
+            time.sleep(5)
+            self.engine.say(txt)
+            self.engine.runAndWait()
         label.destroy()
         if self.f_back == 0:
             self.r_voice()
@@ -75,40 +81,45 @@ class GifLabel(Label):
         if con == 1:
             root.destroy()
             exit()
-
+        
     def Thread_speak(self,*text,res=None,con=None):
+        self.con = con
         text = list(text)
         if res != None:
+            res.split()
             c = 0
             for i in res:
-                text[0] += (i+' ')
+                text[2] += i
+                text[2] += ' '
                 c += 1
-                if c == 35:
-                    text[0] += '\n'
+                if c == 100:
+                    text[2] += '\n'
+                    c=0
         speech = td.Thread(target = self.speak,args = (text,con))
         speech.start()
 
     def r_voice(self):
         self.f_back=2
-        mic = sr.Recognizer()
-        with sr.Microphone() as source:
-            self.Thread_speak("Recognizing...")
-            mic.pause_threshold = 0.8
-            audio = mic.listen(source)
-        try:
-            self.Thread_speak("Wait...")
-            query = self.mic.recognize_google(audio, language='en-in')
-            self.Thread_speak(f"User Side: {query}\n")
-            return query
-        except Exception as e:
-            self.Thread_speak("Say again, please...")
-            return ""
+        if self.con != 1:
+            mic = sr.Recognizer()
+            with sr.Microphone() as source:
+                self.Thread_speak("Wating for the Command...")
+                mic.pause_threshold = 0.8
+                audio = mic.listen(source)
+            try:
+                self.Thread_speak("Wait...")
+                query = self.mic.recognize_google(audio, language='en-in')
+                self.Thread_speak(f"User Side: {query}\n")
+                return query
+            except Exception as e:
+                self.Thread_speak("Say again, please...")
+                return ""
 
     def Run(self,*action,txt_fld=None):
+        
         self.Thread_speak(f'Openning {action[0]}...')
         os.startfile(f'{action[1]}')
         txt_fld.delete('1.0','end')
-        self.entry = 1
 
     def pros(self, query,txt_fld=None):
         query = query.replace(' ','')
@@ -156,7 +167,6 @@ class GifLabel(Label):
             query = query.replace(" ","")
             webbrowser.open(f"google.com/search?q={query}")
             txt_fld.delete('1.0','end')
-            self.entry = 1
 
         elif ("youtube" in query):
             self.Thread_speak("Opening Youtube...")
@@ -164,26 +174,31 @@ class GifLabel(Label):
             query = query.replace(" ","")
             webbrowser.open(f"youtube.com/results?search_query={query}")
             txt_fld.delete('1.0','end')
-            self.entry = 1
 
         elif ("wikipedia" in query):
             mk = mediawiki.MediaWiki()
             query = query.replace("wikipedia","")
             txt_fld.delete('1.0','end')
             try:
-                self.Thread_speak("searching in wikipedia","According to wikipedia",'',res=mk.summary(query,sentences=3).split(' ','.',','))
+                self.Thread_speak("searching in wikipedia","According to wikipedia",'',res=mk.summary(query,sentences=3))
                 
             except mediawiki.DisambiguationError as e:
 
                 self.Thread_speak("Sorry the Information you looking is couldn't find...!")
-            self.entry = 1
+                self.Thread_speak("Give Clear Content..")
+            except Exception:
+                self.Thread_speak("Please Check Your Internet and Try again")
 
         elif ("play" in query):
-            self.Thread_speak("Starting...")
-            query = query.replace("play","")
-            yt.playonyt(query)
-            txt_fld.delete('1.0','end')
-            self.entry = 1
+            
+            try:
+                query = query.replace("play","")
+                yt.playonyt(query)
+                self.Thread_speak("Starting...")
+                txt_fld.delete('1.0','end')
+
+            except Exception:
+                self.Thread_speak("Please Check Your Internet and Try again")
 
         elif ("shutdown" in query) or ("switchoff" in query) or ("poweroff" in query):
             subprocess.run(["shutdown","-s"])
@@ -199,11 +214,10 @@ class GifLabel(Label):
                 for i in res.results:
                     ans+=i.text
                 if ans == '':
-                    self.Thread_speak(text='I cannot Understand what are you trying to Say..')
+                    self.Thread_speak('I cannot find the answer..')
                 else :
-                    self.Thread_speak('',res = ans)
-                self.entry=1
-            except urllib.error.URLError as e:
+                    self.Thread_speak(ans)
+            except urllib.error.URLError :
                 txt_fld.delete('1.0','end')
                 self.Thread_speak("Please Check Your Internet and Try again")
 
@@ -225,51 +239,60 @@ class GifLabel(Label):
         else:
             wish = "Good Evening"
         self.Thread_speak(wish,"I am Michella","How can I help you")
-
+        
     def key(self,keyboard=None,Mic=None,txt_fld=None,submit=None):
-        keyboard.destroy()
-        Mic.destroy()
+        keyboard.pack_forget()
         self.f_back = 1
-        self.welcome()
+        Mic.pack()
         txt_fld.pack()
         submit.pack()
 
-    def mic(self,keyboard=None,Mic=None):
-        keyboard.destroy()
-        Mic.destroy()
-        self.f_back = 0
-        self.welcome()
-        
-    def des_btn(self,btn=None,keyboard=None,Mic=None):
-        btn.destroy()
+    def mic(self,keyboard=None,Mic=None,txt_fld=None,submit=None):
+        Mic.pack_forget()
+        txt_fld.pack_forget()
+        submit.pack_forget()
         keyboard.pack()
-        Mic.pack()
-
+        self.f_back = 0
+        
     def window(self,master=None):
 
-        btn = Button(master,text='Start',width=10)
+        #Destination of the Mic Image
+        mic_ico = PhotoImage(file = "D:\\All Properties\\Python\\Projects\\SDAW\\Addeds\\Mic.png")
+
+        #Destination of the Keyboard Image
+        key_ico = PhotoImage(file = "D:\\All Properties\\Python\\Projects\\SDAW\\Addeds\\Keyboard.png")
 
         txt_fld = Text(master,width=50,height=1,bg='white',fg='black')
 
         submit = Button(master,text='Submit',width=10,command=lambda :self.get_txt(txt_fld=txt_fld))
 
-        keyboard = Button(master,width=8,text='Keyboard',command=lambda:self.key(keyboard=keyboard,Mic=Mic,txt_fld=txt_fld,submit=submit))
+        keyboard = Button(master,image = key_ico,command=lambda:self.key(keyboard=keyboard,Mic=Mic,txt_fld=txt_fld,submit=submit))
 
-        Mic = Button(master,width=8,text='Mic',command=lambda:self.mic(keyboard=keyboard,Mic=Mic))
+        Mic = Button(master,image = mic_ico,command=lambda:self.mic(keyboard=keyboard,Mic=Mic,txt_fld=txt_fld,submit=submit))
 
-        btn.config(command=lambda: self.des_btn(btn=btn,keyboard=keyboard,Mic=Mic))
+        keyboard.pack()
+        
+        self.welcome()
 
-        btn.pack()
-
+        self.r_voice()
+        
+        
 if __name__ == '__main__':
-    global root,btn,txt_fld,submit,keyboard,Mic
+    try:
+        import pywhatkit as yt
+    except Exception as e:
+        pass
+
+    global root
+    
     root = Tk()
     root.geometry('1355x850')
     root.title('Desktop  Assistant')
     root.config(bg='black')
 
     lb = GifLabel(root)
-    lb.image_load('D:\\new.gif')
+    #Destination of the GIF image
+    lb.image_load('D:\\All Properties\\Python\\Projects\\SDAW\\Addeds\\AI Interface.gif')
     lb.pack()
 
     w_thread = td.Thread(target = lambda:lb.window(master=root))
